da140fa47a58879032b117ded96832bdb3577498
Carter Hinsley <carterhinsley@outlook.com>
2015-11-12 16:01:55 +0000

    Add registrant/config.Eval()

--- config/config.go
+++ config/config.go
@@ -1,10 +1,10 @@
+// Package config contains utilities for dealing with configuration data.
 package config
 
-/* TODO: Uncomment this.
 import (
 	"encoding/json"
+	"io"
 )
-*/
 
 type Domain struct {
 	name string
@@ -18,4 +18,13 @@ type Config struct {
 	debug     bool
 	domains   []Domain
 	registrar Endpoint
+}
+
+func Eval(reader io.Reader) (Config, error) {
+	var cfg Config
+	
+	dec := json.NewDecoder(reader)
+	err := dec.Decode(&cfg)
+	
+	return cfg, err
 }
---
+++ config/config_test.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	"io"
+	"testing"
+)
+
+// TestEval determines whether Eval produces a valid Config object from an
+// ad-hoc example file.
+func TestEval(t *testing.T) {
+	var testData string
+	var err error
+	
+	// Define an IO pipe to send/receive data locally
+	reader, writer := io.Pipe()
+	defer reader.Close()
+	defer writer.Close()
+	
+	// First test: Determine whether Eval() produces results from test data.
+	
+	testData = `
+		{
+			"debug": true,
+			"domains": [{
+				"name": "woodnsword.io"
+			}],
+			"registrar": {
+				"address": "woodnsword.net",
+				"port": "7776"
+			}
+		}
+	`
+	go writer.Write([]byte(testData))
+	
+	_, err = Eval(reader)
+	if err != nil {
+		t.Error(err)
+	}
+	
+	// Second test: Determine whether Eval() panics when it receives invalid
+	// test data.
+	
+	testData = `blah blah`
+	go writer.Write([]byte(testData))
+	
+	_, err = Eval(reader)
+	if err == nil {
+		t.Error("Eval() did not produce an error when expected to.")
+	}
+}
