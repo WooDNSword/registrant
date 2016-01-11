17bf4b6936df3e619df47f396cd698cf8f8d15d5
Carter Hinsley <carterhinsley@outlook.com>
2016-01-11 23:12:09 +0000

    Add configuration file reading/decoding

--- registrant.py
+++ registrant.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
 
+import json
+
+# TODO: Move read_cfg to a specialized module.
+# TODO: Document read_cfg.
+def read_cfg(file_path):
+	with open(file_path) as f:
+		return json.loads(f.read())
+
 if __name__ == "__main__":
-	print("Hello, world!")
+	cfg = read_cfg('res/json/cfg.json')
+	# TODO: Initiate connection to WDNS Registrar.
