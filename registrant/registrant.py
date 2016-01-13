ec79dfe544661c9fde414227283823f387841bad
Carter Hinsley <carterhinsley@outlook.com>
2016-01-13 00:09:19 +0000

    Add TODO for moving cfg loading to shared repo

--- registrant/registrant.py
+++ registrant/registrant.py
@@ -7,6 +7,8 @@ import socket
 
 
 if __name__ == '__main__':
+	# TODO: Move cfg loading logic into a separate module/repo (exists in both
+	# registrar and registrant).
 	# os.path module shit deals with Python's idiotic file path mechanisms.
 	cfg_data = cfg.read_cfg(
 		os.path.dirname(
